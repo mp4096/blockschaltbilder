737b8dd2ad1d6d14af95cf7fa15be3b0e2f490f7
Mikhail Pak <mikhail.pak@tum.de>
2017-03-26 17:20:35 +0200

    I didn't expect a kind of PEP8 inquisition

--- generate_boilerplate.py
+++ generate_boilerplate.py
@@ -1,10 +1,14 @@
 import argparse
-from blockschaltbilder import *
+from blockschaltbilder import convert_to_tikz
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Convert *.bsb file(s) into boilerplate TikZ file(s).")
-    parser.add_argument("paths", metavar="p", type=str, nargs='*', default=".",
-                        help="""specifies the location of files or folders to be converted
-                        (default: convert all in the current folder and it subfolders)""")
+    parser = argparse.ArgumentParser(
+        description="Convert *.bsb file(s) into boilerplate TikZ file(s).",
+        )
+    parser.add_argument(
+        "paths", metavar="p", type=str, nargs='*', default=".",
+        help="""specifies the location of files or folders to be converted
+        (default: convert all in the current folder and it subfolders)""",
+        )
     args = parser.parse_args()
     convert_to_tikz(args.paths)
